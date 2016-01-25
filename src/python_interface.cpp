6ab3127b4a5fc8c933e98d65f063ed7455d21ed7
Scott Ivey <scott.ivey@gmail.com>
2016-01-24 19:19:51 -0500

    python interface updates

--- src/python_interface.cpp
+++ src/python_interface.cpp
@@ -5,6 +5,9 @@
 #include "hashing/ConstantSpaceSimHasher.h"
 #include "hashing/hash_funcs.h"
 #include "util/misc.h"
+#include "Language.h"
+#include "language_detection/GlobalLanguageDetector.h"
+#include "language_detection/GlobalLanguageProfiles.h"
 
 using namespace std;
 
@@ -31,6 +34,24 @@ uint64_t hamming_distance(uint64_t x, uint64_t y) {
   return util::hammingDistance(x, y);
 }
 
+void init_global_language_profiles_from_file(std::string fileName) {
+  language_detection::GlobalLanguageProfiles::initFromFile(fileName);
+}
+
+void init_global_language_profiles_from_json(std::string jsonData) {
+  language_detection::GlobalLanguageProfiles::initFromFile(jsonData);
+}
+
+void destroy_global_language_profiles() {
+  language_detection::GlobalLanguageProfiles::destroy();
+}
+
+std::string detect_language(std::string text) {
+  language_detection::GlobalLanguageDetector detector;
+  auto lang = detector.detect(text, 7);
+  return stringOfLanguage(lang);
+}
+
 }} // texty::python_interface
 
 
@@ -43,6 +64,17 @@ PYBIND11_PLUGIN(textypy) {
     m.def("simhash", &texty::python_interface::simhash, "Compute simhash score of 2-shingles for given text");
     m.def("rotate_bits", &texty::python_interface::rotate_bits, "Get a vector of rotations of a 64-bit value");
     m.def("hamming_distance", &texty::python_interface::hamming_distance, "Get hamming distance of two 64-bit values");
-
+    m.def("init_global_language_profiles_from_file",
+        &texty::python_interface::init_global_language_profiles_from_file,
+        "initialize global language detection from a file path, which should contain valid JSON");
+    m.def("init_global_language_profiles_from_json",
+        &texty::python_interface::init_global_language_profiles_from_json,
+        "initialize global language detection profiles from a raw JSON string");
+    m.def("destroy_global_language_profiles",
+        &texty::python_interface::destroy_global_language_profiles,
+        "free memory used by the global language detection profiles");
+    m.def("detect_language",
+        &texty::python_interface::detect_language,
+        "Detect most likely language of UTF-8 or ASCII text.");
     return m.ptr();
 }
